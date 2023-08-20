a4f9246eb39b4eda19a9bb13d5240dd744d8145f
Melusi2022 <melusimedia@gmail.com>
2023-08-20 10:36:40 -0700

    printf

--- _printf.c
+++ _printf.c
@@ -1,51 +1,70 @@
 #include "main.h"
+#define BUFF_SIZE 1024
+
+
 void print_buffer(char buffer[], int *buff_ind);
-/**
- * _printf - Printf function
- * @format: format.
- * Return: Printed chars.
- */
+
+
 int _printf(const char *format, ...)
 {
-	int i, printed = 0, printed_chars = 0;
-	int flags, width, precision, size, buff_ind = 0;
+	int i, printed_chars = 0, buff_ind = 0;
 	va_list list;
 	char buffer[BUFF_SIZE];
 
 	if (format == NULL)
 		return (-1);
-	va_start(list, format);
-	for (i = 0; format && format[i] != '\0'; i++)
-	if (buff_ind == BUFF_SIZE)
-	{
-		print_buffer(buffer, &buff_ind);
-	printed_chars++;
-	}
-	else
-	{
-		print_buffer(buffer, &buff_ind);
 
-		flags = get_flags(format, &i);
-		width = get_width(format, &i, list);
-		precision = get_precision(format, &i, list);
-		size = get_size(format, &i);
-		i++;
-	printed = handle_print(format, &i, list, buffer, flags, width, precision, size);
-	if (printed == -1)
-	return (-1);
+	va_start(list, format);
+	for (i = 0; format[i] != '\0'; i++)
+	{
+		if (buff_ind == BUFF_SIZE - 1)
+		{
+			print_buffer(buffer, &buff_ind);
+			printed_chars += buff_ind;
+		}
+		if (format[i] == '%')
+		{
+			i++;
+			if (format[i] == 'c')
+			{
+				char c = (char)va_arg(list, int);
+				buffer[buff_ind++] = c;
+			}
+
+			else if (format[i] == 's')
+				{
+					char *str  = va_arg(list, char *);
+					int j;
+					for (j = 0; str[j] != '\0'; j++)
+					{
+						if (buff_ind == BUFF_SIZE - 1)
+						{
+							print_buffer(buffer, &buff_ind);
+							printed_chars += buff_ind;
+						}
+						buffer[buff_ind++] = str[j];
+					}
+				}
+				else if (format[i] == '%')
+				{
+					buffer[buff_ind++] = '%';
+				}
+		}
+		else
+		{
+			buffer[buff_ind++] = format[i];
+		}
 	}
-		print_buffer(buffer, &buff_ind);
+
+	print_buffer(buffer, &buff_ind);
+	va_end(list);
+	return (printed_chars);
 }
-/**
- * print_buffer - Prints the contents of the buffer if it exist
- * @buffer: input value
- * @buff_ind: input value
- */
 void print_buffer(char buffer[], int *buff_ind)
 {
 	if (*buff_ind > 0)
-	write(1, &buffer[0], *buff_ind);
-
-	*buff_ind = 0;
+	{
+		write(1, buffer, *buff_ind);
+		*buff_ind = 0;
+	}
 }
-
---
+++ functions.h
@@ -0,0 +1,8 @@
+#ifndef FUNCTIONS_H
+#define FUNCTIONS_H
+
+void print_char(char c);
+void print_string(const char *str);
+void print_percent(void);
+
+#endif
