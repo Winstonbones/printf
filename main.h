7e7e67fc5b9addf3a5ad36e9f150047cb59c6a3f
Melusi2022 <melusimedia@gmail.com>
2023-08-21 02:40:59 -0700

    printf

--- main.h
+++ main.h
@@ -2,17 +2,10 @@
 #define MAIN_H
 #include <stdarg.h>
 #include <stddef.h>
-<<<<<<< HEAD
 #include "functions.h"
 #include <unistd.h>
 void print_buffer(char buffer[], int *buff_ind);
 int _printf(const char *format, ...);
-
-
-
-
-=======
 int _printf(const char *format, ...);
 void print_buffer(char buffer[], int *buff_ind);
->>>>>>> eb2342d1d88bef7631bcb4aa22e9d65612e454c4
 #endif
