a8a1f0ad67cf4713d0b2efcbe537fa9127aa63ba
winstonbones <remothabetsej@gmail.co>
2023-08-23 02:43:09 -0700

    printf

--- _printf.c
+++ _printf.c
@@ -1,87 +1,58 @@
+#include "printf_helpers.h"
+#include <stdarg.h>
+#include <unistd.h>
+#include <string.h>
 #include "main.h"
 
+void print_buffer(char buffer[], int *buff_ind);
+
 /**
- * _printf - printf function
- * @format: format
- *
- * Return: printed chars
+ * _printf - Printf function
+ * @format: format.
+ * Return: Printed chars.
  */
 int _printf(const char *format, ...)
 {
-	int i, printed_chars = 0, buff_ind = 0, len = 0;
+	int i, printed = 0, printed_chars = 0;
+	int flags, width, precision, size, buff_ind = 0;
 	va_list list;
 	char buffer[BUFF_SIZE];
-	
+
 	if (format == NULL)
 		return (-1);
+
 	va_start(list, format);
-	for (i = 0; format[i] != '\0'; i++)
-		if (format[i] != '%')
+
+	for (i = 0; format && format[i] != '\0'; i++)
 	{
-		if (buff_ind == BUFF_SIZE - 1)
+		if (format[i] != '%')
 		{
-			print_buffer(buffer, &buff_ind);
-			printed_chars += buff_ind;
+			buffer[buff_ind++] = format[i];
+			if (buff_ind == BUFF_SIZE)
+				print_buffer(buffer, &buff_ind);
+			/* write(1, &format[i], 1);*/
+			printed_chars++;
 		}
-		buffer[buff_ind++] = format[i];
-		len++;
-	}
 		else
 		{
-			i++;
-			if (format[i] == 'c')
-			{
-				char c = (char)va_arg(list, int);
-				buffer[buff_ind++] = c;
-			}
-			else if
-				(format[i] == 's')
-				{
-					char *str = va_arg(list, char *);
-					int j;
-					for (j = 0; str[j] != '\0'; j++)
-					{
-						if (buff_ind == BUFF_SIZE - 1)
-						{
-							print_buffer(buffer, &buff_ind);
-							printed_chars += buff_ind;
-						}
-						buffer[buff_ind++] = str[j];
-					}
-					len +=j;
-
-				}
-			else if (format[i] == '%')
-			{
-				buffer[buff_ind++] = '%';
-				len++;
-			}
-			else if (format[i] == 'd')
-			{
-				len += handle_integer(list, buffer, buff_ind);
-			}
-			else if (format[i] == 'f')
-			{
-				len += handle_float(list, buffer, buff_ind);
-			}
-			else
-			{
-				buffer[buff_ind++] = '%';
-				buffer[buff_ind++] = format[i];
-				len += 2;
-			}
+			print_buffer(buffer, &buff_ind);
+			flags = get_flags(format, &i);
+			width = get_width(format, &i, list);
+			precision = get_precision(format, &i, list);
+			size = get_size(format, &i);
+			++i;
+			printed = handle_print(format, &i, list, buffer,
+				flags, width, precision, size);
+			if (printed == -1)
+				return (-1);
+			printed_chars += printed;
 		}
+	}
 
 	print_buffer(buffer, &buff_ind);
+
 	va_end(list);
+
 	return (printed_chars);
 }
 
-void print_buffer(char buffer[], int *buff_ind)
-{
-        if (*buff_ind > 0)
-        {
-                write(1, buffer, *buff_ind);
-                *buff_ind = 0;
-        }
-}
