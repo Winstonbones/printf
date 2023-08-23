2cdc0b9e0b353c60922113c2b3ddf0bfa1189d27
Melusi2022 <melusimedia@gmail.com>
2023-08-23 13:33:18 -0700

    printf

--- functions1.c
+++ functions1.c
@@ -15,6 +15,7 @@ int flags, int width, int precision, int size)
 {
 	int i = BUFF_SIZE - 2;
 	unsigned long int num = va_arg(types, unsigned long int);
+
 	num = convert_size_unsgnd(num, size);
 	if (num == 0)
 	buffer[i--] = '0';
@@ -45,21 +46,21 @@ int flags, int width, int precision, int size)
 	int i = BUFF_SIZE - 2;
 	unsigned long int num = va_arg(types, unsigned long int);
 	unsigned long int init_num = num;
+
 	UNUSED(width);
 	num = convert_size_unsgnd(num, size);
 
 	if (num == 0)
-	buffer[i--] = '0';
-	buffer[BUFF_SIZE - 1] = '\0';
+		buffer[i--] = '0';
+		buffer[BUFF_SIZE - 1] = '\0';
 	while (num > 0)
 		{
 			buffer[i--] = (num % 8) + '0';
 			num /= 8;
 		}
 			if (flags & F_HASH && init_num != 0)
-			buffer[i--] = '0';
+				buffer[i--] = '0';
 			i++;
-			
 			return (write_unsgnd(0, i, buffer, flags, width, precision, size));
 }
 
@@ -119,6 +120,7 @@ int flags, char flag_ch, int width, int precision, int size)
 	int i = BUFF_SIZE - 2;
 	unsigned long int num = va_arg(types, unsigned long int);
 	unsigned long int init_num = num;
+
 	UNUSED(width);
 	num = convert_size_unsgnd(num, size);
 	if (num == 0)
